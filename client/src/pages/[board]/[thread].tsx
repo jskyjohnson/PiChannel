798f5f71182123a130d2e305ffef4675fc6bab1f
nullmage <jsky.johnson@gmail.com>
2021-01-20 00:47:47 -0700

    Adds basic pages and logo

---
+++ client/src/pages/[board]/[thread].tsx
@@ -0,0 +1,12 @@
+
+
+//Should only return when there's a valid thread... else 404?
+const Thread = () => {
+	return (
+		<div>
+			this is a Thread
+		</div>
+	);
+}
+
+export default Thread;
---
+++ client/src/pages/blog/index.tsx
@@ -0,0 +1,9 @@
+const index = () => {
+	return (
+		<div>
+			This is not a board!
+		</div>
+	);
+}
+
+export default index;
